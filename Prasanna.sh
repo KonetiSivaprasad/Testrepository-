9824b6ba9cc0302c045d171c1bc1637f915fbeb2
ec2-user <ksp.sivprasad@gmail.com>
2026-07-22 18:55:50 +0530

    commit all files

--- Prasanna.sh
+++ Prasanna.sh
@@ -2,7 +2,14 @@ echo "---------Start--------"
 hostname
 date
 df -h
+<<<<<<< HEAD
 pvs
 vgs
 lvs
+=======
+sudo pvs
+sudo vgs
+sudo lvs
+timedatectl
+>>>>>>> e3ab5c4 (commit all files)
 echo "--------end----------"
